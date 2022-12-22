e0cfdae385a9ba9e53720afe5b207cbccb591d9e
haroon <haroon357@hotmail.com>
2022-12-22 19:02:38 +0000

    draft

--- src/main/java/com/haroon/lambda/invoker/CustomerLambdaInvoker.java
+++ src/main/java/com/haroon/lambda/invoker/CustomerLambdaInvoker.java
@@ -10,6 +10,7 @@ import java.util.List;
 public interface CustomerLambdaInvoker {
 
     //'test' is the name of lambda function we want to invoke
+    //The lambda function test is returning the List of Customer as json
     @LambdaFunction(functionName = "test")
     List<Customer> getCustomer();
 
